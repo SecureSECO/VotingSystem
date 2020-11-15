25830ed21d1599f41e1a8c4abf1925c9a9a9c579
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-11-15 15:28:10 +0330

    Updates

---
+++ precache-manifest.3943bc7f6c313058053fac351aa74f2f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5510f6687c88a972c7e97df453216927",
+    "url": "/Votingsystem/index.html"
+  },
+  {
+    "revision": "36eaab7872f8d35c8e32",
+    "url": "/Votingsystem/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "565e88541bd71636d216",
+    "url": "/Votingsystem/static/css/main.d74023b6.chunk.css"
+  },
+  {
+    "revision": "36eaab7872f8d35c8e32",
+    "url": "/Votingsystem/static/js/2.518e87b2.chunk.js"
+  },
+  {
+    "revision": "4be4126eca2c6dd6ccb4d17c1ab10ab2",
+    "url": "/Votingsystem/static/js/2.518e87b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565e88541bd71636d216",
+    "url": "/Votingsystem/static/js/main.f81a15ad.chunk.js"
+  },
+  {
+    "revision": "317c1b5ad761eae8bc1e",
+    "url": "/Votingsystem/static/js/runtime-main.9706176a.js"
+  },
+  {
+    "revision": "856ac9d0115fa9abbc7581e22a53994b",
+    "url": "/Votingsystem/static/media/bg2.856ac9d0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Votingsystem/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b0d6fa0093865cad7c48d1c8ff6ccbed",
+    "url": "/Votingsystem/static/media/mylogo.b0d6fa00.jpg"
+  },
+  {
+    "revision": "0f04b257c666e13481655674eb6ad85d",
+    "url": "/Votingsystem/static/media/uulogo.0f04b257.png"
+  }
+]);
